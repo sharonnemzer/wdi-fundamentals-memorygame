6806954da901450f8946e1ba1fb5ef20f073bfbb
Sharon Nemzer <sharonnemzer@Sharons-MacBook-Pro.local>
2018-02-03 17:07:16 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,23 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
-cardsInPlay = [];
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+ console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+};
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cardId);
-  var checkForMatch = function () {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
-    } else {
-      console.log("Sorry, try again.");
-checkForMatch();
-    }}
-  }
-  flipCard(0);
-  flipCard(2);
+ console.log("User flipped " + cards[cardId]);
+ cardsInPlay.push(cards[cardId]);
+ if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
+    {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+checkForMatch()};
+};
+
+flipCard(0);
+flipCard(2);
